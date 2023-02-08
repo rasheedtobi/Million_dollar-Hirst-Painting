a5c1d5056f7a8728ac2ce7791f3ef6927f76446e
Rasheed Odunbaku <tobirasheed@gmail.com>
2023-02-08 16:27:08 +0100

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+import colorgram
+import turtle
+import random
+
+
+te = turtle.Turtle()
+turtle.colormode(255)
+te.penup()
+te.hideturtle()
+te.speed('fastest')
+
+
+def extract_colors(filename, num):
+    colors = colorgram.extract(filename, num)
+    rgb_colors = []
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        n_color = (r, g, b)
+        rgb_colors.append(n_color)
+    # print(rgb_colors)
+    return rgb_colors
+
+
+te.setheading(230)
+te.forward(300)
+te.setheading(0)
+
+spots_num = 100
+for dot in range(1, spots_num+1):
+    color_list = extract_colors('hirst.jpg', 36)
+    te.dot(20, random.choice(color_list))
+    te.forward(50)
+
+    if dot % 10 == 0:
+        te.setheading(90)
+        te.forward(50)
+        te.setheading(180)
+        te.forward(500)
+        te.setheading(0)
+
+
+screen = turtle.Screen()
+screen.exitonclick()
